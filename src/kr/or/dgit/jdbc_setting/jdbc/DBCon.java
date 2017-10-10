55bf198e5976e5de69652c91bd9cd3e9f6a4ae9c
Mosquito(A) <skykim10908@naver.com>
2017-10-10 15:05:17 +0900

    20171010 수업 중

--- src/kr/or/dgit/jdbc_setting/jdbc/DBCon.java
+++ src/kr/or/dgit/jdbc_setting/jdbc/DBCon.java
@@ -3,6 +3,8 @@ package kr.or.dgit.jdbc_setting.jdbc;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Properties;
 
 
@@ -23,11 +25,20 @@ public class DBCon {
 	private DBCon() {
 											// 앞서 resources 폴더에 만든 파일을 매개파일로 넘겨줌
 		Properties properties = getProperties("conf.properties");
-		System.out.println(properties.getProperty("user"));
+		try {
+			connection = DriverManager.getConnection(properties.getProperty("url"), properties.getProperty("user"), properties.getProperty("pwd"));
+		} catch (SQLException e) {
+			System.err.printf("%s - %s%n", e.getErrorCode(), e.getMessage());
+			e.printStackTrace();
+		}
+		
+		
+		/*System.out.println(properties.getProperty("user"));
 		System.out.println(properties.getProperty("pwd"));
-		System.out.println(properties.getProperty("url"));
+		System.out.println(properties.getProperty("url"));*/
 	}
 
+		//DB에 접속하기 위한 ID, PWD, URL 등을 읽어들이는 메소드
 	private Properties getProperties(String propertiesPath) {
 		Properties properties = new Properties();
 		InputStream is = ClassLoader.getSystemResourceAsStream(propertiesPath);
